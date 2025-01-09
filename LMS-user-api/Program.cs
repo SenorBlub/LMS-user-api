83efc72d9247f239c4b53daa26c4587a22792955
SenorBlub <thomasverhappen@Gmail.com>
2025-01-09 12:02:02 +0100

    logto added

--- LMS-user-api/Program.cs
+++ LMS-user-api/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
 });
 
 var connectionString =
-	$"Server={Env.GetString("DB_HOST")};Database={Env.GetString("DB_NAME")};Uid={Env.GetString("DB_USER")};Pwd={Env.GetString("DB_PASSWORD")};";
+	$"Server={Env.GetString("DB_HOST")};Database={Env.GetString("DB_NAME")};User Id={Env.GetString("DB_USER")};Password={Env.GetString("DB_PASSWORD")};";
 
 Console.WriteLine(connectionString);
 
@@ -38,7 +38,8 @@ builder.Services.AddDbContext<UserDbContext>(options =>
 		connectionString,
 		new MySqlServerVersion(new Version(Env.GetInt("SQL_MAJOR"), Env.GetInt("SQL_MINOR"), Env.GetInt("SQL_BUILD"))),
 		mySqlOptions => mySqlOptions.EnableRetryOnFailure()
-	)
+
+	).LogTo(Console.WriteLine)
 );
 
 //!TODO auth
